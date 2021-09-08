424b6e2707f8111cc433f9c174c025cc0fec2a2e
Koen Bok <koen@framer.com>
2021-09-08 17:02:25 +0200

    test

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,7 @@
 import { Pulse } from "./index";
 
+console.log("test")
+
 window["Pulse"] = (scope: string, source: string) => {
   return new Pulse(scope, source, `https://beat.dev/${scope}`);
 };
